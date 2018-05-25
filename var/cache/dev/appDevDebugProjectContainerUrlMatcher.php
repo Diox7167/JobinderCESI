e773c334fa669c65f31e1a986571d7796881f108
BERNARD Ghislain <bernard.ghislain@viacesi.fr>
2018-05-25 14:16:53 +0200

    Accueil

--- var/cache/dev/appDevDebugProjectContainerUrlMatcher.php
+++ var/cache/dev/appDevDebugProjectContainerUrlMatcher.php
@@ -107,25 +107,20 @@ class appDevDebugProjectContainerUrlMatcher extends Symfony\Bundle\FrameworkBund
 
         }
 
-        // homepage
+        // accueil
         if ('' === $trimmedPathinfo) {
-            $ret = array (  '_controller' => 'AppBundle\\Controller\\DefaultController::indexAction',  '_route' => 'homepage',);
+            $ret = array (  '_controller' => 'AppBundle\\Controller\\DefaultController::accueilAction',  '_route' => 'accueil',);
             if ('/' === substr($pathinfo, -1)) {
                 // no-op
             } elseif ('GET' !== $canonicalMethod) {
-                goto not_homepage;
+                goto not_accueil;
             } else {
-                return array_replace($ret, $this->redirect($rawPathinfo.'/', 'homepage'));
+                return array_replace($ret, $this->redirect($rawPathinfo.'/', 'accueil'));
             }
 
             return $ret;
         }
-        not_homepage:
-
-        // accueil
-        if ('/accueil' === $pathinfo) {
-            return array (  '_controller' => 'AppBundle\\Controller\\DefaultController::accueilAction',  '_route' => 'accueil',);
-        }
+        not_accueil:
 
         if ('/' === $pathinfo && !$allow) {
             throw new Symfony\Component\Routing\Exception\NoConfigurationException();
