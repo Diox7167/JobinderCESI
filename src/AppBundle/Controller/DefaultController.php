2ef7acc517784ad1dd2f2f3ab4c7e995638e0fd0
BERNARD Ghislain <bernard.ghislain@viacesi.fr>
2018-06-05 09:40:54 +0200

    Ajout pages
    Ajout page Membre et Resultats

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,4 +19,26 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/membre", name="membre")
+     */
+    public function membreAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/membre.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/resultats", name="resultats")
+     */
+    public function resultatsAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/resultats.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
 }
