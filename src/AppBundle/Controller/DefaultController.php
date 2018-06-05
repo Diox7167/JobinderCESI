ee95bf992c21230db904116234ec9389259a31a8
Kevin Le Croller <lecroller@hotmail.fr>
2018-06-05 10:10:31 +0200

    correction inscription + ajout controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,4 +19,16 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function accueilInscription(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/inscription.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
 }
